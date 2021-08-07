bb2e5125c94e6004a740d7ed3d2cd6cc420f3b06
Hendrik Roth <hendrik@die5roths.de>
2021-08-07 16:03:25 +0200

    added docstrings in Numpy Style for FileHandler class

--- filehandler.py
+++ filehandler.py
@@ -3,7 +3,27 @@ import pandas as pd
 
 
 class FileHandler:
+    """
+    This class represents all operations for handling a file.
+
+    Attributes
+    ----------
+    filename : str
+        name of the file
+    file_path : str
+        path of the file as String
+    """
+
     def __init__(self, filename):
+        """
+        Inits the FileHandler. Set the filename and gives the file
+        the right filepath from the directory 'statistant'
+
+        Parameters
+        ----------
+        filename : str
+            is given name of the file
+        """
         self.filename = filename
 
         # init file path
@@ -16,6 +36,14 @@ class FileHandler:
         return self.file_path
 
     def read_csv(self):
+        """
+        function for reading the file as csv
+
+        Returns
+        -------
+        df : DataFrame
+            DataFrame of reading result
+        """
         path = self.file_path
         df = pd.read_csv(path)
         return df
