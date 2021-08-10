0e9a12488feae00ab8cb2d7d12b7f33c337d9074
Hendrik Roth <hendrik@die5roths.de>
2021-08-10 18:50:17 +0200

    improved interactions 2 and 4 of mean function

--- __init__.py
+++ __init__.py
@@ -26,35 +26,22 @@ class Statistant(MycroftSkill):
     @intent_file_handler('mean.intent')
     def handle_mean(self, message):
         filename = message.data.get('file')
-        col = message.data.get('col').lower()
+        col = message.data.get('searching').lower()
 
         try:
             file_handler = FileHandler(filename)
             df = file_handler.content
-            mean = round(df[col].mean(), 3)
+            row1 = None
+            row2 = None
 
-            self.speak_dialog('mean', {'col': col, 'avg': mean})
-        except FileNotFoundError:
-            self.speak_dialog('FileNotFound.error', {'filename': filename})
-        except FileNotUniqueError:
-            self.speak_dialog('FileNotUnique.error', {'filename': filename})
+            if message.data.get('search_one') is not None:
+                row1 = w2n.word_to_num(message.data.get('search_one'))
+                row2 = w2n.word_to_num(message.data.get('search_two'))
+                mean = round(df.loc[df.index[row1:row2], col].mean(), 3)
+            else:
+                mean = round(df[col].mean(), 3)
 
-    @intent_file_handler('mean2.intent')
-    def handle_mean_rows(self, message):
-        filename = message.data.get('file')
-        col = message.data.get('col').lower()
-
-        # TODO think of solution for index -> will user say index or will be row "one" = index 0?
-        row1 = w2n.word_to_num(message.data.get('row1'))
-        row2 = w2n.word_to_num(message.data.get('row2'))
-
-        try:
-            file_handler = FileHandler(filename)
-            df = file_handler.content
-
-            mean = round(df.loc[df.index[row1:row2], col].mean(), 3)
-
-            self.speak_dialog('mean2', {'avg': mean})
+            self.speak_dialog('mean', {'col': col, 'avg': mean, "search_one": row1, "search_two": row2})
         except FileNotFoundError:
             self.speak_dialog('FileNotFound.error', {'filename': filename})
         except FileNotUniqueError:
