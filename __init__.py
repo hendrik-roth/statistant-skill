89ad13eb477aebb131c796108941482dd959a1cc
Hendrik Roth <hendrik@die5roths.de>
2021-08-10 20:32:43 +0200

    improved interactions 2 and 4 of mean function

--- __init__.py
+++ __init__.py
@@ -26,7 +26,7 @@ class Statistant(MycroftSkill):
     @intent_file_handler('mean.intent')
     def handle_mean(self, message):
         filename = message.data.get('file')
-        col = message.data.get('searching').lower()
+        col = message.data.get('colname').lower()
 
         try:
             file_handler = FileHandler(filename)
@@ -35,16 +35,16 @@ class Statistant(MycroftSkill):
             row2 = None
 
             # if search_one is not none -> intent with rows
-            if message.data.get('search_one') is not None:
+            if message.data.get('lower') is not None:
 
                 # user will more likely say to index=0 row=1, etc. -> sub -1
-                row1 = w2n.word_to_num(message.data.get('search_one'))
-                row2 = w2n.word_to_num(message.data.get('search_two'))
+                row1 = w2n.word_to_num(message.data.get('lower'))
+                row2 = w2n.word_to_num(message.data.get('upper'))
                 mean = round(df.loc[df.index[(row1 - 1):row2], col].mean(), 3)
             else:
                 mean = round(df[col].mean(), 3)
 
-            self.speak_dialog('mean', {'col': col, 'avg': mean, "search_one": row1, "search_two": row2})
+            self.speak_dialog('mean', {'colname': col, 'avg': mean})
         except FileNotFoundError:
             self.speak_dialog('FileNotFound.error', {'filename': filename})
         except FileNotUniqueError:
