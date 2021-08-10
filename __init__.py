8dbe7f2b6e12d72bce62c8515244c974e0e33157
Hendrik Roth <hendrik@die5roths.de>
2021-08-10 16:20:09 +0200

    added interactions 2 and 4 of mean function

--- __init__.py
+++ __init__.py
@@ -1,6 +1,8 @@
 from adapt.intent import IntentBuilder
 from mycroft import MycroftSkill, intent_file_handler, intent_handler
 import os
+from word2number import w2n
+
 from .filehandler import FileHandler
 from .exceptions import FileNotUniqueError
 
@@ -29,6 +31,27 @@ class Statistant(MycroftSkill):
             mean = round(df[col].mean(), 3)
 
             self.speak_dialog('mean', {'col': col, 'avg': mean})
+        except FileNotFoundError:
+            self.speak_dialog('FileNotFound.error', {'filename': filename})
+        except FileNotUniqueError:
+            self.speak_dialog('FileNotUnique.error', {'filename': filename})
+
+    @intent_file_handler('mean2.intent')
+    def handle_mean_rows(self, message):
+        filename = message.data.get('file')
+        col = message.data.get('col').lower()
+
+        # TODO think of solution for index -> will user say index or will be row "one" = index 0?
+        row1 = w2n.word_to_num(message.data.get('row1'))
+        row2 = w2n.word_to_num(message.data.get('row2'))
+
+        try:
+            file_handler = FileHandler(filename)
+            df = file_handler.content
+
+            mean = round(df.loc[df.index[row1:row2], col].mean(), 3)
+
+            self.speak_dialog('mean2', {'avg': mean})
         except FileNotFoundError:
             self.speak_dialog('FileNotFound.error', {'filename': filename})
         except FileNotUniqueError:
